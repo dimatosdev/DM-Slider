bd04fbf33ad63e1ea391d95f7c63652a908aef48
dimatosdev <dimatosdev@gmail.com>
2025-04-07 15:52:55 -0300

    Add Metaboxes

--- wp-content/plugins/dm-slider/post-types/class.dm-slider-cpt.php
+++ wp-content/plugins/dm-slider/post-types/class.dm-slider-cpt.php
@@ -6,7 +6,11 @@ if ( ! class_exists('DM_Slider_Post_Type') ) {
             add_action( 'init', array( $this, 'create_post_type' ) );
             add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
             add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+            add_filter( 'manage_dm-slider_posts_columns', array( $this, 'dm_slider_cpt_columns' ) );
+            add_action( 'manage_dm-slider_posts_custom_column', array( $this, 'dm_slider_custom_columns' ), 10, 2 );
+            add_filter( 'manage_edit-dm-slider_sortable_columns', array( $this, 'dm_slider_sortable_columns' ) );
         }
+        
         public function create_post_type() {
             register_post_type( 'dm-slider', array(
                 'label' => 'DM Slider',
@@ -33,6 +37,29 @@ if ( ! class_exists('DM_Slider_Post_Type') ) {
             ) );
         }
 
+        public function dm_slider_cpt_columns( $columns ) {
+            $columns['dm_slider_link_text'] = esc_html__( 'Link Text', 'dm-slider' );
+            $columns['dm_slider_link_url'] = esc_html__( 'Link URL', 'dm-slider' );
+            return $columns;
+        }
+
+        public function dm_slider_custom_columns( $column, $post_id ) {
+            switch( $column ) {
+                case 'dm_slider_link_text':
+                    echo esc_html(get_post_meta( $post_id, 'dm_slider_link_text', true )); 
+                    break;
+                case 'dm_slider_link_url':
+                    echo esc_url(get_post_meta( $post_id, 'dm_slider_link_url', true ));
+                    break;
+            }
+        }
+
+        public function dm_slider_sortable_columns( $columns ) {
+            $columns['dm_slider_link_text'] = 'dm_slider_link_text';
+            $columns['dm_slider_link_url'] = 'dm_slider_link_url';
+            return $columns;
+        }
+
         public function add_meta_boxes() {
             add_meta_box(
                 'dm_slider_meta_box',
@@ -49,9 +76,42 @@ if ( ! class_exists('DM_Slider_Post_Type') ) {
         }
 
         public function save_post( $post_id ) {
+            if( isset( $_POST['dm_slider_nonce'] ) ) {
+                if( ! wp_verify_nonce( $_POST['dm_slider_nonce'], 'dm_slider_nonce' ) ) {
+                    return;
+                }
+            }
+
+            if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+                return;
+            }
+
+            if( isset( $_POST['post_type'] ) && $_POST['post_type'] === 'dm-slider' ) {
+                if( ! current_user_can( 'edit_post', $post_id ) ) {
+                    return;
+                } else if ( ! current_user_can( 'edit_page', $post_id ) ) {
+                    return;
+                }
+            }
+
             if( isset( $_POST['action'] ) && $_POST['action'] == 'editpost' ) {
-                $link_text = sanitize_text_field( $_POST['dm-slider-link-text'] );
-                update_post_meta( $post_id, 'dm-slider-link-text', $link_text );
+                $old_link_text = get_post_meta( $post_id, 'dm_slider_link_text', true );
+                $new_link_text = $_POST['dm-slider-link-text'];
+                $old_link_url = get_post_meta( $post_id, 'dm_slider_link_url', true );
+                $new_link_url = $_POST['dm-slider-link-url'];
+
+                if( empty( $new_link_text ) ) {
+                    update_post_meta( $post_id, 'dm_slider_link_text', 'Add some text' );
+                }else {
+                    update_post_meta( $post_id, 'dm_slider_link_text', sanitize_text_field($new_link_text), $old_link_text );
+                }
+
+                if( empty( $new_link_url ) ) {
+                    update_post_meta( $post_id, 'dm_slider_link_url', 'http://' );
+                }else {
+                    update_post_meta( $post_id, 'dm_slider_link_url', esc_url_raw($new_link_url), $old_link_url );
+                }
+
             }
         }
     }    
--- wp-content/plugins/dm-slider/views/dm-slider-metabox.php
+++ wp-content/plugins/dm-slider/views/dm-slider-metabox.php
@@ -1,18 +1,19 @@
 <table class="form-table dm-slider-metabox">
+    <input type="hidden" name="dm_slider_nonce" value="<?php echo wp_create_nonce( "dm_slider_nonce" ); ?>">
     <tbody>
         <tr>
             <th>
-                <label for="dm-slider-link-text">Link</label>
+                <label for="dm-slider-link-text">Link Text</label>
             </th>
             <td>
-                <input 
+            <input 
                 type="text" 
                 name="dm-slider-link-text" 
                 id="dm-slider-link" 
                 class="regular-text link-input" 
-                value=""
+                value="<?php echo esc_attr( get_post_meta( $post->ID, 'dm_slider_link_text', true ) ); ?>"
                 required
-                >
+            >
             </td>
         </tr>
         <tr>
@@ -20,14 +21,14 @@
                 <label for="dm-slider-link-url">Link URL</label>
             </th>
             <td>
-                <input
+            <input
                 type="url"
                 name="dm-slider-link-url"
                 id="dm-slider-link-url"
                 class="regular-text link-url"
-                value=""
+                value="<?php echo esc_url( get_post_meta( $post->ID, 'dm_slider_link_url', true ) ); ?>"
                 required
-                >
+            >
             </td>
         </tr>
     </tbody>
